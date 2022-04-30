3492cc230378e59e0a9e5afc3709af61fef878b5
dovlin_m <dovlin.m@gmail.com>
2022-04-30 16:27:07 +0300

    changes import

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const userName = (name) => {
-    var userName = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + userName + '!');
+const userName = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
+
+export default userName;
