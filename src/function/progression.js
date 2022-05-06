35565d89ff1817e09ccc9857dd65670ea3d7c3e3
dovlin_m <dovlin.m@gmail.com>
2022-05-06 16:29:35 +0300

    refactoring

--- src/function/progression.js
+++ src/function/progression.js
@@ -6,14 +6,16 @@ const creatingProgression = () => {
   let progression = numberRandom(1, 30);
   arr.push(progression);
   const dif = numberRandom(1, 5);
-  const shadow = numberRandom(1, length + 1);
+  const shadow = numberRandom(1, length);
 
   for (let i = 1; i < length; i += 1) {
     progression += dif;
     arr.push(progression);
+    console.log(arr);
   }
 
   const an = arr[shadow];
+  console.log(an);
   arr[shadow] = '..';
   let question = '';
 
